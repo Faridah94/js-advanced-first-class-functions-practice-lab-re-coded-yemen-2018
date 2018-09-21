61ade9620791f9e81bef553c6a0e65fecb57a721
Farida <faridah.absi@gmail.com>
2018-09-21 19:19:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+
+function  logDriverNames(drivers) {
+  
+drivers.forEach(function(element) {
+  console.log(element.name);
+})
+
+}
+
+function logDriversByHometown(drivers, argument){
+  drivers.forEach(function(elem) {
+    
+  if (elem.hometown === argument){
+      console.log(elem.name);
+    }
+})
+}
+
+function driversByRevenue(drivers){
+  let newArr = [];
+  
+  drivers.forEach(function(element){
+    newArr.push(element)
+  })
+  newArr.sort(function(a, b) {
+  return a.revenue - b.revenue;
+});
+ return newArr;
+}
+
+ function driversByName(drivers){
+   let newArr = [];
+  
+    drivers.forEach(function(element){
+    newArr.push(element)
+  })
+  
+ newArr.sort(function (a,b){
+    return a.name.localeCompare(b.name);
+  })
+  return newArr;
+    
+}
+
+function totalRevenue(drivers){
+var sum = drivers.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
+
+
+}
